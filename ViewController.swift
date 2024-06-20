ffe295312595f782b57c046ee546aa83c4014d82
Nastaran Rakhshan <80073157+Nas26@users.noreply.github.com>
2024-06-20 11:22:15 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Nastaran Rakhshan on 2024-06-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
